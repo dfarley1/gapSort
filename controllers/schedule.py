b3e65f742fc6782030c854b9396261dc46e42572
kyle <kyjoconn@ucsc.edu>
2016-11-12 13:45:01 -0800

    times for inbetween days of multi-day events should be correct now, added comments

--- controllers/schedule.py
+++ controllers/schedule.py
@@ -8,22 +8,35 @@ def index(): return dict(message="hello from schedule.py")
 
 @auth.requires_login()
 def add():
-    #look up all events that belong to this user
-    #return them all
+    # if present, grab event id from URL
     record = db.events(request.args(0)) or None
 
+    # Create a form to add an event to the database
     form = SQLFORM(db.events, record, deleteable=True, fields=['start_time', 'end_time', 'description', 'name'],showid=False)
     
+    #autofill the user_id since users won't know their own user id
     form.vars.user_id = auth.user.id
-    #does this event span multiple days?
+    
+    #process the form once it is submitted
     if form.process().accepted:
+        #take down the starting and ending times of this event
+        #first day already got stored when the form was accepted so start one day later
         start_time = form.vars.start_time + datetime.timedelta(days=1)
         end_time = form.vars.end_time
-        escape = 0
-        while start_time.date() <= end_time.date() or escape > 10:
-            db.events.insert(user_id = auth.user.id, start_time = start_time, end_time = end_time, description=form.vars.description, name=form.vars.description)
-            start_time = start_time + datetime.timedelta(days=1)
-            escape += 1
+        #does this event span multiple days?
+        #num_days = (end_time - start_time).days()
+        which_day = 1
+        while start_time.date() <= end_time.date():
+            #is this the last day?
+            if start_time.date() == end_time.date():
+                starts_at_midnight = start_time.replace(hour=0, minute=0)
+                db.events.insert(user_id = auth.user.id, start_time = starts_at_midnight, end_time = end_time, description=form.vars.description, name=form.vars.description)
+                start_time = start_time + datetime.timedelta(days=1)
+            else:
+                starts_at_midnight = start_time.replace(hour=0, minute=0)
+                midnight_tomorrow = (start_time + datetime.timedelta(days=1)).replace(hour=0, minute=0)
+                db.events.insert(user_id = auth.user.id, start_time = starts_at_midnight, end_time = midnight_tomorrow, description=form.vars.description, name=form.vars.description)
+                start_time = start_time + datetime.timedelta(days=1)
         if record:
             redirect('../../default/index')
         redirect('../default/index')
